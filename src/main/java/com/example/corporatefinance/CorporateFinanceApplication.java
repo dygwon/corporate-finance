aa9512e8ebd1cfdeb35695c963818b183692d3b2
Daniel Gwon <dygwon@gmail.com>
2023-10-04 21:25:13 -0400

    Initial commit

---
+++ src/main/java/com/example/corporatefinance/CorporateFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.corporatefinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corporatefinance/CorporateFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corporatefinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
